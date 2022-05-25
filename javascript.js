e02e074a95286f7e9c591a60bb5cc3afca714af6
Niko Shin <nshin17@gmail.com>
2022-05-24 21:39:21 -0700

    Updated all operator functions to use the es6 rest parameter to take in n number of arguments

--- javascript.js
+++ javascript.js
@@ -1,30 +1,38 @@
 
 //todo: Creating functions for math operators (add, subtract, multiply, divide):
-const add = function add(num1, num2) {
-    return (num1 + num2);
+const add = function add(...num) {
+    return num.reduce((total, current) => {
+        return (total + current);
+    })
 }
-// console.log(add(2, 2)); // 4
+// console.log(add(2, 2, 3, 5, 1));
 
-const subtract = function subtract(num1, num2) {
-    return (num1 - num2);
+const subtract = function subtract(...num) {
+    return num.reduce((total, current) => {
+        return (total - current);
+    })
 }
-// console.log(subtract(4, 2)); // 2
+// console.log(subtract(14, 2, 2, 2));
 
-const multiply = function multiply(num1, num2) {
-    return (num1 * num2);
+const multiply = function multiply(...num) {
+    return num.reduce((total, current) => {
+        return (total * current);
+    })
 }
 // console.log(multiply(5, 5)); // 25
 
-const divide = function divide(num1, num2) {
-    return (num1 / num2);
+const divide = function divide(...num) {
+    return num.reduce((total, current) => {
+        return (total / current);
+    })
 }
-// console.log(divide(10, 2)); // 5
+// console.log(divide(10, 2, 2, 10)); // 5
 
 //todo: Create a new function 'operate' that takes an operator and 2 numbers and then calls one of the above functions on the numbers:
 function operate(operator, num1, num2) {
     return (operator(num1, num2));
 }
-// console.log(operate(multiply, 10, 2));
+console.log(operate(multiply, 10, 2));
 
 //todo: Create the functions that populate the display when you click the number buttons... you should be storing the 'display value' in a variable somewhere for use in the next step:
 // input display with button (number or symbol that is clicked by user).
@@ -33,10 +41,10 @@ function operate(operator, num1, num2) {
 
 // const button = document.querySelectorAll('button').forEach(element => element.click());
 let userInput = []; // stores all the numbers pressed by the user.    
+const numberRange = [...Array(10).keys()]; // range of numbers in array.
 const button = document.querySelectorAll('button');
 const textField = document.getElementById('text-field');
 
-const numberRange = [...Array(10).keys()];
 
 function buttonInput() {
     for (let i=0; i<button.length; i++) {
@@ -47,7 +55,9 @@ function buttonInput() {
                 // console.log(button[i]);
                 textField.value = userInput.join(""); // removes all commas from 'userInput' array.
             });
+        } else if (button[i].value === 'add') {
+            return operate(add, userInput);
         }
     }
 }    
-buttonInput();
+// buttonInput();
